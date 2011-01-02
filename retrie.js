5eebb7787d05e6dbdc52c5ec0732f21bf5f18bc2
satyr <murky.satyr@gmail.com>
2011-01-02 17:45:26 +0900

    refactored and rebuilt with --bare

--- retrie.js
+++ retrie.js
@@ -1,70 +1,66 @@
-(function(){
-  (typeof exports != 'undefined' && exports !== null ? exports : this).Retrie = (function(){
-    var _proto = Retrie.prototype;
-    function _ctor(){} _ctor.prototype = _proto;
-    function Retrie(strings, asPrefixes){
-      var str, _i, _len, _this = new _ctor;
-      _this.tree = {};
-      if (strings) {
-        for (_i = 0, _len = strings.length; _i < _len; ++_i) {
-          str = strings[_i];
-          _this.add(str, asPrefixes);
+(typeof exports != 'undefined' && exports !== null ? exports : this).Retrie = (function(){
+  var _proto = Retrie.prototype;
+  function _ctor(){} _ctor.prototype = _proto;
+  function Retrie(strings, asPrefixes){
+    var str, _i, _len, _this = new _ctor;
+    _this.tree = {};
+    if (strings) {
+      for (_i = 0, _len = strings.length; _i < _len; ++_i) {
+        str = strings[_i];
+        _this.add(str, asPrefixes);
+      }
+    }
+    return _this;
+  } Retrie.name = 'Retrie';
+  _proto.add = function(string, asPrefixes){
+    var ref, i, _to, _name;
+    ref = this.tree;
+    for (i = 0, _to = string.length; i < _to; ++i) {
+      ref = ref[_name = string.charAt(i)] || (ref[_name] = asPrefixes
+        ? {
+          '': ''
         }
-      }
-      return _this;
-    } Retrie.name = 'Retrie';
-    _proto.add = function(string, asPrefixes){
-      var ref, i, _to, _name;
-      ref = this.tree;
-      for (i = 0, _to = string.length; i < _to; ++i) {
-        ref = ref[_name = string.charAt(i)] || (ref[_name] = asPrefixes
-          ? {
-            '': ''
-          }
-          : {});
-      }
-      if (!asPrefixes) {
-        ref[''] = '';
-      }
-      return this;
-    };
-    _proto.toString = function(){
-      var meta;
-      meta = /[.?*+^$|()\{\[\\]/;
-      function recur(it){
-        var chr, alt, cc, q, sub, cconly, re;
-        LEAF_CHECK:
-        if ('' in it) {
-          for (chr in it) {
-            if (chr) {
-              break LEAF_CHECK;
-            }
-          }
-          return '';
-        }
-        alt = [];
-        cc = [];
+        : {});
+    }
+    if (!asPrefixes) {
+      ref[''] = '';
+    }
+    return this;
+  };
+  _proto.toString = function(){
+    function recur(it){
+      var chr, alt, cc, q, sub, cconly, re;
+      LEAF_CHECK:
+      if ('' in it) {
         for (chr in it) {
-          if (!chr) {
-            q = true;
-            continue;
+          if (chr) {
+            break LEAF_CHECK;
           }
-          ((sub = recur(it[chr])) ? alt : cc).push(chr.replace(meta, '\\$&') + sub);
         }
-        cconly = !alt.length;
-        cc.length && alt.push(cc[1]
-          ? "[" + cc.join('') + "]"
-          : cc[0]);
-        re = alt[1]
-          ? "(?:" + alt.join('|') + ")"
-          : alt[0];
-        q && (re = cconly
-          ? re + '?'
-          : "(?:" + re + ")?");
-        return re || '';
+        return '';
       }
-      return recur(this.tree);
-    };
-    return Retrie;
-  }());
-}).call(this);
+      alt = [];
+      cc = [];
+      for (chr in it) {
+        if (!chr) {
+          q = true;
+          continue;
+        }
+        ((sub = recur(it[chr])) ? alt : cc).push((~'.?*+^$|(){[\\'.indexOf(chr) ? '\\' + chr : chr) + sub);
+      }
+      cconly = !alt.length;
+      cc.length && alt.push(cc[1]
+        ? "[" + cc.join('') + "]"
+        : cc[0]);
+      re = alt[1]
+        ? "(?:" + alt.join('|') + ")"
+        : alt[0];
+      q && (re = cconly
+        ? re + '?'
+        : "(?:" + re + ")?");
+      return re || '';
+    }
+    return recur(this.tree);
+  };
+  return Retrie;
+}());
